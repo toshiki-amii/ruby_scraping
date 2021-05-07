3e48d337b26c3bae0258db027b025c31a62a5c35
toshiki-amii <80087083+toshiki-amii@users.noreply.github.com>
2021-05-07 19:00:29 +0900

    オプションの実装

--- scrape.rb
+++ scrape.rb
@@ -1,6 +1,20 @@
 require 'net/http'
 require 'nokogiri'
 require 'json'
+require 'optparse'
+
+opt = OptionParser.new
+opt.on('--infile=VAL')
+opt.on('--outfile=VAL')
+opt.on('--category=VAL')
+
+params = {}
+opt.parse!(ARGV, into: params)
+
+if params[:infile] && params[:category]
+  puts "Error: --infile と --category は同時に指定できません。"
+  exit(1)
+end
 
 def get_from(url)
   Net::HTTP.get(URI(url))
@@ -19,13 +33,30 @@ end
 
 def scrape_section(section)
   {
-    category: section.xpath('./h6').first.text,
+    category: section.xpath('./h6').text,
     news:  section.xpath('./div/div').map { |node| scrape_news(node) }
   }
 end
 
-html = File.read('pitnews.html') 
+if params[:infile]
+  html = File.read(params[:infile])
+else
+  url = 'https://masayuki14.github.io/pit-news/'
+  if params[:category]
+    url = url + '?category=' + params[:category]
+  end 
+  html = get_from(url)
+end
+
 doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+
 pitnews = doc.xpath('/html/body/main/section[position() > 1]').map { |section| scrape_section(section) }
-write_file('pitnews.json', {pitnews: pitnews}.to_json)
+
+if params[:outfile]
+  outfile = params[:outfile]
+else
+  outfile = 'pitnews.json'
+end
+
+write_file(outfile, {pitnews: pitnews}.to_json)
 
