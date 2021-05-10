38ac18ec84d74e8f67216208179bba084ef3e050
toshiki-amii <80087083+toshiki-amii@users.noreply.github.com>
2021-05-10 11:51:52 +0900

    ソースファイルの分割

---
+++ lib/mercury/scraper.rb
@@ -0,0 +1,27 @@
+require 'nokogiri'
+
+module Mercury
+
+  class Scraper
+  
+    def self.scrape_news(news)
+      {
+        title: news.xpath('./p/strong/a').first.text,
+        url: news.xpath('./p/strong/a').first['href']
+      }
+    end
+    
+    def self.scrape_section(section)
+      {
+        category: section.xpath('./h6').text,
+        news:  section.xpath('./div/div').map { |node| scrape_news(node) }
+      }
+    end
+  
+    def self.scrape(html)
+      doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+      doc.xpath('/html/body/main/section[position() > 1]').map { |section| scrape_section(section) }
+    end
+  end
+  
+end
